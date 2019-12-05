c436cf2fa29a92267878eb235f24fe1fc060b171
cander <cander@example.com>
2019-12-05 22:47:41 +0800

    增加sql

--- go/startmysql.sh
+++ go/startmysql.sh
@@ -2,4 +2,4 @@
 
 # docker 安装mysql
 # linux 建议用host网络
-docker run --restart=always -v /data/mysql:/var/lib/mysql --net=host -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=project -d mysql:5.7.26
+docker run --restart=always -v /data/mysql:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=project -d mysql:5.7.26
