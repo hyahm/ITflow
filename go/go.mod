50a6c52dcc3a14feca446f541c5c2ece9f658c96
cander <cander_liubiao@126.com>
2020-05-07 18:09:07 +0800

    add count

--- go/go.mod
+++ go/go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/hyahm/cachetable v0.0.0-20200507073204-abe535c45d36
+	github.com/hyahm/cachetable v0.0.0-20200507074722-20249a71c18b
 	github.com/hyahm/goconfig v0.0.0-20200411075020-85401a1b9350
 	github.com/hyahm/golog v0.0.0-20200319122124-04b0a58a15ff
 	github.com/hyahm/gomysql v0.0.5-0.20191228040128-27a4e3456547
--- go/route/log.go
+++ go/route/log.go
@@ -15,9 +15,12 @@ func init() {
 
 	Log.Pattern("/search/log").Post(handle.SearchLog).Bind(&log.Search_log{}).
 		AddMidware(midware.JsonToStruct).
-		ApiReqStruct(&log.Search_log{})
+		ApiDescribe("条件搜索日志列表").
+		ApiReqStruct(&log.Search_log{}).ApiRequestTemplate(`{"page": 1, "limit": 10, "classify":"login"}`).
+		ApiResStruct(&log.Loglist{}).ApiResponseTemplate(`{"loglist":[{"id":26,"exectime":1588840365,"classify":"login","content":"","ip":"127.0.0.1"},{"id":25,"exectime":1588840233,"classify":"login","content":"","ip":"127.0.0.1"},{"id":24,"exectime":1588837232,"classify":"login","content":"","ip":"127.0.0.1"},{"id":23,"exectime":1588837002,"classify":"login","content":"","ip":"127.0.0.1"},{"id":22,"exectime":1588833133,"classify":"login","content":"","ip":"127.0.0.1"},{"id":21,"exectime":1588833047,"classify":"login","content":"","ip":"127.0.0.1"}],"code":0, "count":100}`)
 
-	Log.Pattern("/log/classify").Post(handle.LogClassify)
+	Log.Pattern("/log/classify").Post(handle.LogClassify).
+		ApiDescribe("获取classify列表")
 
 	Log.Pattern("/log/list").Post(handle.LogList).Bind(&log.Search_log{}).
 		AddMidware(midware.JsonToStruct).
