4f13faf2cac1fc63388660d3453328d1ec436b78
cander <cander_liubiao@126.com>
2020-08-22 15:52:30 +0800

    升级gomysql包

--- go/db/sql.go
+++ go/db/sql.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hyahm/goconfig"
 	"github.com/hyahm/golog"
@@ -12,12 +14,14 @@ var Mconn *gomysql.Db
 func InitMysql() {
 	var err error
 	conf := &gomysql.Sqlconfig{
-		DbName:   goconfig.ReadString("mysql.db", "project"),
-		Host:     goconfig.ReadString("mysql.host", "127.0.0.1"),
-		UserName: goconfig.ReadString("mysql.user", "root"),
-		Password: goconfig.ReadPassword("mysql.pwd", "123456"),
-		Port:     goconfig.ReadInt("mysql.port", 3306),
-		Timeout:  "5s",
+		DbName:          goconfig.ReadString("mysql.db", "project"),
+		Host:            goconfig.ReadString("mysql.host", "127.0.0.1"),
+		UserName:        goconfig.ReadString("mysql.user", "root"),
+		Password:        goconfig.ReadPassword("mysql.pwd", "123456"),
+		Port:            goconfig.ReadInt("mysql.port", 3306),
+		Timeout:         time.Second * 5,
+		ReadTimeout:     time.Second * 30,
+		ConnMaxLifetime: time.Hour * 4,
 	}
 	Mconn, err = conf.NewDb()
 	if err != nil {
--- go/go.mod
+++ go/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/hyahm/cachetable v0.0.0-20200707100838-5dd5455ce3d5
 	github.com/hyahm/goconfig v0.0.0-20200814144712-bd712352a305
 	github.com/hyahm/golog v0.0.0-20200806030423-4f17fcb54e88
-	github.com/hyahm/gomysql v0.0.0-20200820065334-ca30cc71be7f
+	github.com/hyahm/gomysql v0.0.0-20200822074832-d1312fb2276a
 	github.com/hyahm/xmux v0.0.0-20200817090937-90aef5b9d018
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
