fdd199be2936f86b5032fbbabd860149a98c3c49
cander <cander@htdtek.com>
2021-02-01 23:32:08 +0800

    test

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -35,8 +35,7 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
