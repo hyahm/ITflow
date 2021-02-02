b1454b2e2ac8a83d4aaadb502123230330fa79d4
cander <cander_liubiao@126.com>
2021-02-02 17:44:12 +0800

    title

--- vue/src/utils/get-page-title.js
+++ vue/src/utils/get-page-title.js
@@ -1,10 +1,10 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || 'ITflow'
 
 export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
+    if (pageTitle) {
+        return `${pageTitle} - ${title}`
+    }
+    return `${title}`
 }
