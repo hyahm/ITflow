5d302650ce1a4fb2e71bf3871d0240aac5d70b6d
cander <cander_liubiao@126.com>
2021-02-04 18:11:30 +0800

    a

--- go/handle/email.go
+++ go/handle/email.go
@@ -23,7 +23,7 @@ func TestEmail(w http.ResponseWriter, r *http.Request) {
 	if getemail.Nickname == "" {
 		getemail.Nickname = strings.Split(getemail.EmailAddr, "@")[0]
 	}
-	mail.TestMail(getemail.Host, getemail.EmailAddr, getemail.Password, getemail.Port, getemail.To)
+	mail.TestMail(getemail.Host, getemail.EmailAddr, getemail.Password, getemail.Port, getemail.To, getemail.Nickname)
 	send, _ := json.Marshal(errorcode)
 	w.Write(send)
 	return
