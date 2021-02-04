e14aecabeafa24383e0469eff254c26714778fd7
cander <cander_liubiao@126.com>
2021-02-04 18:10:28 +0800

    a

--- go/mail/mail.go
+++ go/mail/mail.go
@@ -7,14 +7,16 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
-func TestMail(host, username string, password string, port int, touser string) error {
+func TestMail(host, username string, password string, port int, touser, nickname string) error {
 	d := gomail.NewDialer(host, port, username, password)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-
+	if nickname == "" {
+		nickname = strings.Split(username, "@")[0]
+	}
 	m := gomail.NewMessage()
 	m.SetHeader("From", username)
 	m.SetHeader("To", strings.Split(touser, ",")...)
-	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetAddressHeader("Cc", username, nickname)
 	m.SetHeader("Subject", "验证您的邮箱是否能收到邮件")
 	m.SetBody("text/html", "恭喜， 您的邮箱可以使用")
 	// m.Attach("/home/Alex/lolcat.jpg")
